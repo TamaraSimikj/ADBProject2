93fc06e0ece327bdc49b871395310997536c374c
Tamara Simikj <tamara.simic12@gmail.com>
2023-06-26 20:16:47 +0200

    making appointment/2

--- src/main/java/com/beautycenter/adbproject2/AdbProject2Application.java
+++ src/main/java/com/beautycenter/adbproject2/AdbProject2Application.java
@@ -2,6 +2,10 @@ package com.beautycenter.adbproject2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class AdbProject2Application {
@@ -9,5 +13,9 @@ public class AdbProject2Application {
     public static void main(String[] args) {
         SpringApplication.run(AdbProject2Application.class, args);
     }
-
+//
+//    @Bean
+//    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+//        return new JdbcTemplate(dataSource);
+//    }
 }
--- src/main/java/com/beautycenter/adbproject2/web/AppointmentController.java
+++ src/main/java/com/beautycenter/adbproject2/web/AppointmentController.java
@@ -1,11 +1,12 @@
 package com.beautycenter.adbproject2.web;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -33,21 +34,23 @@ public class AppointmentController {
         String sql = "SELECT id, service_category, \"value\" FROM \"final\".services WHERE bcID =" +id;
         List<Map<String,Object>> services =jdbcTemplate.queryForList(sql);
         model.addAttribute("services", services);
+        model.addAttribute("id", id);
         return "services";
     }
 
-    @PostMapping("/submit-services")
-    public String submitSelectedServices(@RequestParam(name = "selectedServices", required = false) List<Integer> selectedServices,
-                                         @RequestParam(name = "beautyCenterId") int beautyCenterId) {
-        // Process the selected services
+    @PostMapping("/services/{id}")
+    public String submitSelectedServices(
+            @PathVariable int id,
+            @RequestParam(name = "selectedServices") List<Integer> selectedServices)
+    {
         if (selectedServices != null && !selectedServices.isEmpty()) {
             // Convert the list of selected service IDs to a query parameter string
             String serviceIds = selectedServices.stream()
                     .map(String::valueOf)
                     .collect(Collectors.joining(","));
 
-            // Redirect to the employeelistfromservices view with the selected services and beauty center ID as query parameters
-            return "redirect:/employeelistfromservices?services=" + serviceIds + "&beautyCenterId=" + beautyCenterId;
+            String encodedServiceIds = URLEncoder.encode(serviceIds, StandardCharsets.UTF_8);
+            return "redirect:/employeelistfromservices?bcId=" + id + "&services=" + encodedServiceIds;
         } else {
             // No services selected, handle the case accordingly
             return "redirect:/services?error";
@@ -56,15 +59,50 @@ public class AppointmentController {
 
     @GetMapping("/employeelistfromservices")
     public String getEmployeesForServices(@RequestParam(name = "services") String serviceIds,
-                                          @RequestParam(name = "beautyCenterId") int beautyCenterId,
+                                          @RequestParam(name = "bcId") int bcId,
                                           Model model) {
-        // Retrieve employees for the selected services and beauty center ID from the database
-        String sql = "SELECT * FROM employeesForServices WHERE bcId = ? AND serviceId IN (" + serviceIds + ")";
-        List<Map<String, Object>> employees = jdbcTemplate.queryForList(sql, beautyCenterId);
 
-        // Add the employees to the model
+        String sql = "SELECT * FROM \"final\".employeesForServices WHERE bcId = ? AND id IN (" + serviceIds + ")";
+        List<Map<String, Object>> employees = jdbcTemplate.queryForList(sql, bcId);
+
         model.addAttribute("employees", employees);
+        model.addAttribute("services", serviceIds);
 
         return "employeelistfromservices";
     }
+
+    @GetMapping("/booking-times")
+    public String getBookingTimes(
+            @RequestParam("bcId") int beautyCenterId,
+            @RequestParam("employeeId") int employeeId,
+            @RequestParam("services") String serviceIds,
+            Model model
+    ) {
+        String encodedServiceIds = URLEncoder.encode(serviceIds, StandardCharsets.UTF_8);
+
+        // Fetch the booking times based on the provided parameters
+        String sql = "SELECT * FROM \"final\".free_appointments WHERE employeeuserid = ?" +
+                " AND BeautyCenterID =" + beautyCenterId +
+            " AND ServiceID IN (" + serviceIds +")";
+        List<Map<String, Object>> bookingTimes = jdbcTemplate.queryForList(sql, employeeId);
+
+        // Pass the booking times to the Thymeleaf template
+        model.addAttribute("bookingTimes", bookingTimes);
+
+        return "booking-times";
+    }
+
+    @PostMapping("/booking-time")
+    public String submitBookingTime(@RequestParam("bookingTimeId") int bookingTimeId) {
+        // Process the selected booking time
+        System.out.println("Selected booking time ID: " + bookingTimeId);
+
+        // Redirect or handle the processing result accordingly
+        return "redirect:/booking-confirmation?bookingTimeId=" + bookingTimeId;
+    }
+
+
+
+
+
 }
